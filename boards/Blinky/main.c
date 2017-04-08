b05959f27159e469e8d7a0f05aa4b13f6e49ec57
nrivkin <noah.rivkin@students.olin.edu>
2017-04-08 11:12:42 -0400

    Commiting changes to blinky code in preparation for updating branch

--- boards/Blinky/main.c
+++ boards/Blinky/main.c
@@ -5,12 +5,16 @@
 int main (void) {
     // Set PE1 to output
     // Use pin 10 to light up an LED
-    DDRE |= _BV(PE1);
+    // DDRE |= _BV(PE1);
+    DDRD |= _BV(PD6);
+    DDRD |= _BV(PD7);
 
     while(1) {
         // Toggle PE1 (pin 10)
         // Toggles power to pin 10 to create a "blink"
-        PORTE ^= _BV(PE1);
+        PORTD ^= _BV(PD6);
+        _delay_ms(1000);
+        PORTD ^= _BV(PD7);
 
         // Give a delay to the toggle so it doesn't infinitely toggle
         _delay_ms(500);
