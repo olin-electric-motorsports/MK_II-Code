696ad78a435bb666f42a7aaae94b1319e70c014c
Byron Wasti <byron.wasti@gmail.com>
2017-04-16 13:44:26 -0400

    SPI test

--- boards/H4CK3RB04rD_left/main.c
+++ boards/H4CK3RB04rD_left/main.c
@@ -1,33 +1,25 @@
-/* CAN Tx */
+// LEFT
 #define F_CPU (4000000L)
 #include <avr/io.h>
 #include <util/delay.h>
-#include "can_api.h"
 
 int main (void) {
-    // Set PE1 to output
-    // Use pin 10 to light up an LED
     DDRD |= _BV(PD7) | _BV(PD6) | _BV(PD5);
-    PORTD |= _BV(PD7);
-    CAN_init(0, 0);
 
+    DDRB |= _BV(PB1) | _BV(PB7); // MOSI | SCK
+    SPCR |= _BV(SPE) | _BV(MSTR) | _BV(SPR0);
 
-    uint8_t msg[] = { 0x11, 0x66, 0x0a };
+    uint8_t msg = 0x3f;
     while(1) {
         PORTD ^= _BV(PD6);
-        // Toggle PE1 (pin 10)
-        // Toggles power to pin 10 to create a "blink"
-        //PORTD ^= _BV(PD6);
 
-        // Give a delay to the toggle so it doesn't infinitely toggle
-        _delay_us(200);
-        if( bit_is_set(CANGSTA, TXBSY) ) {
-            PORTD |= _BV(PD5);
-        } else {
-            //PORTD &= ~_BV(PD5);
+        SPDR = msg;
+        while( !( SPSR & _BV(SPIF) ) )
+        {
+            PORTD |= _BV(PD7);
         }
+        PORTD &= ~_BV(PD7);
 
-        CAN_Tx( 0, IDT_GLOBAL, IDT_GLOBAL_L, msg );
-
+        _delay_ms(1000);
     }
 }
--- boards/H4CK3RB04rD_right/main.c
+++ boards/H4CK3RB04rD_right/main.c
@@ -1,41 +1,40 @@
-/* RIGHT */
+// RIGHT
 #define F_CPU (4000000L)
 #include <avr/io.h>
 #include <util/delay.h>
-#include "can_api.h"
 
-ISR(CAN_INT_vect)
-{
-
-    CANPAGE = 0x00; // Reset canpage
-    uint8_t msg = CANMSG;
-    if (msg == 0x11){
-        PORTB |= _BV(PB6);
-        //_delay_ms(1);
-        //PORTB &= ~_BV(PB6);
-    } else {
-        PORTD |= _BV(PD0);
-        //_delay_ms(1);
-        //PORTD &= ~_BV(PD0);
-    }
-    
-    CAN_Rx(0, IDT_GLOBAL, IDT_GLOBAL_L, IDM_single);
-}
 
 int main (void) {
-    // Set PE1 to output
-    // Use pin 10 to light up an LED
     DDRB |= _BV(PB5) | _BV(PB6);
     DDRD |= _BV(PD0);
-    CAN_init(0, 0);
 
-    CAN_Rx(0, IDT_GLOBAL, IDT_GLOBAL_L, IDM_single);
+    DDRB |= _BV(PB0); // MISO
+    SPCR |= _BV(SPE);
+
     while(1) {
-        // Toggle PE1 (pin 10)
-        // Toggles power to pin 10 to create a "blink"
-        //PORTB ^= _BV(PB5);
+        PORTB ^= _BV(PB5);
 
-        // Give a delay to the toggle so it doesn't infinitely toggle
-        //_delay_ms(500);
+
+        if( bit_is_set(SPSR, SPIF) )
+        {
+            PORTB |= _BV(PB6);
+
+            /*
+            uint8_t msg = SPDR;
+            if (msg == 0x3f)
+            {
+                PORTD |= _BV(PD0);
+            }
+            */
+        }
+        else 
+        {
+            PORTD |= _BV(PD0);
+        }
+
+        _delay_ms(500);
+        PORTB &= ~_BV(PB6);
+        PORTD &= ~_BV(PD0);
+        _delay_ms(500);
     }
 }
