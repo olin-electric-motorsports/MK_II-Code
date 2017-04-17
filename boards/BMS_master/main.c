e51dcc28b181103e68937d46f82852dbbbe3369b
Riley Chapman <rlychpmn@gmail.com>
2017-04-16 22:57:55 -0400

    basic ATmega blinky

--- boards/BMS_master/main.c
+++ boards/BMS_master/main.c
@@ -3,21 +3,19 @@
 #include <util/delay.h>
 
 int main (void) {
-    // Set PB4 to output
-    // Use pin 10 to light up an LED
+    // Set PB5,PB6,PC0 to output
     DDRB |= _BV(PB5);
     DDRB |= _BV(PB6);
     DDRC |= _BV(PC0);
 
     while(1) {
-        // Toggle PE1 (pin 10)
-        // Toggles power to pin 10 to create a "blink"
+        // Toggle PB5,PB6,PC0
         PORTB ^= _BV(PB5);
         PORTB ^= _BV(PB6);
         PORTC ^= _BV(PC0);
         
 
-        // Give a delay to the toggle so it doesn't infinitely toggle
+        // Give a delay to the toggle so it is visible
         _delay_ms(500);
     }
 }
