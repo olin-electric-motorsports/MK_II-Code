a48b1204a4aaf0660f7437a2d413c1ad55cf7d23
MargaretRosner <rosner.margaret@gmail.com>
2017-04-24 23:12:35 -0400

    can 'talks'

--- boards/Throttle_Brake/main.c
+++ boards/Throttle_Brake/main.c
@@ -1,18 +1,37 @@
-#define F_CPU (1000000L)
+#define F_CPU (4000000L)
 #include <avr/io.h>
 #include <util/delay.h>
+#include "can_api.h"
+
 
 int main (void) {
+
+  /* Most basic CAN transmission.
+   * Sends the bytes 0x11, 0x66 and
+   * 0x0a and then quits. */
+
+  // Initialize CAN
+  CAN_init(0, 0);
+
+  // Set the array msg to contain 3 bytes
+  uint8_t msg[] = {0xF1};
+
+
+  // Transmit message
+
+
     // Set PE1 to output
     // Use pin 10 to light up an LED
-    DDRB |= _BV(PB5);
+    DDRB |= _BV(PB2);
 
     while(1) {
         // Toggle PE1 (pin 10)
         // Toggles power to pin 10 to create a "blink"
-        PORTB ^= _BV(PB5);
+        CAN_Tx( 0, IDT_GLOBAL, IDT_GLOBAL_L, msg );
+        PORTB ^= _BV(PB2);
+        _delay_ms(500);
 
         // Give a delay to the toggle so it doesn't infinitely toggle
-        _delay_ms(500);
+
     }
 }
--- boards/Transom/main.c
+++ boards/Transom/main.c
@@ -1,19 +1,53 @@
 #define F_CPU (4000000L)
 #include <avr/io.h>
 #include <util/delay.h>
+#include "can_api.h"
+
+ISR(CAN_INT_vect)
+{
+    // Reset CANPAGE (A necessary step; don't worry about it
+    CANPAGE = 0x00;
+
+    // msg now contains the first byte of the CAN message.
+    // Repeat this call to receive the other bytes.
+    uint8_t msg = CANMSG;
+    if (msg == 0xF1){
+      PORTC ^= _BV(PC6);
+      //PORTC ^= _BV(PC0);
+    }
+
+    // Set the chip to wait for another message.
+    CAN_Rx(0, IDT_GLOBAL, IDT_GLOBAL_L, IDM_single);
+    PORTC ^= _BV(PC0);
+}
 
 int main (void) {
+
+  /* Most basic CAN transmission.
+   * Sends the bytes 0x11, 0x66 and
+   * 0x0a and then quits. */
+
+  // Initialize CAN
+  CAN_init(0, 0);
+
+  // Set the array msg to contain 3 bytes
+  uint8_t msg[] = {0xF1};
+
+  // Transmit message
+  //CAN_Tx( 0, IDT_GLOBAL, IDT_GLOBAL_L, msg );
+  CAN_Rx(0, IDT_GLOBAL, IDT_GLOBAL_L, IDM_single);
+
+
+
     // Set PE1 to output
     // Use pin 10 to light up an LED
+    DDRC |= _BV(PC6);
     DDRC |= _BV(PC0);
 
     while(1) {
         // Toggle PE1 (pin 10)
         // Toggles power to pin 10 to create a "blink"
-        PORTC ^= _BV(PC0);
-        
-
         // Give a delay to the toggle so it doesn't infinitely toggle
-        _delay_ms(500);
+
     }
 }
