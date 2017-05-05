aada4f98ccb861b3c3343236df29184dc99dc4e7
Byron Wasti <byron.wasti@gmail.com>
2017-05-04 20:50:46 -0400

    Example SPI implementation

--- boards/SPI_master/spi_master.c
+++ boards/SPI_master/spi_master.c
@@ -2,46 +2,103 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <string.h>
+#include <stdio.h>
 #include "lcd.h"
 
-ISR(SPI_STC_vect)
+uint8_t FLAG = 0x00;
+uint8_t BUTTON_STATES = 0x00;
+
+ISR(PCINT0_vect)
 {
-    lcd_puts("Spi Interrupt");
+    if (bit_is_set(PINB, PB1))
+    {
+        BUTTON_STATES &= ~_BV(0);
+    }
+    else
+    {
+        BUTTON_STATES |= _BV(0);
+    }
+
+    if (bit_is_set(PINB, PB0))
+    {
+        BUTTON_STATES &= ~_BV(1);
+    }
+    else
+    {
+        BUTTON_STATES |= _BV(1);
+    }
+}
+
+void init_spi_master(void)
+{
+    // Setup SPI IO pins (MOSI and SCK)
+    DDRD |= _BV(PD3) | _BV(PD4);
+
+    // Enable SPI
+    SPCR |=  _BV(SPE) | _BV(MSTR) | _BV(SPR0);
+
+    // Enable alternate SPI (MISO_A, MOSI_A, etc)
+    // Take this out if you are using standard pins!
+    MCUCR |= _BV(SPIPS);
+}
+
+uint8_t spi_message(uint8_t msg)
+{
+    // Set message
+    SPDR = msg;
+
+    // Wait for transmission to finish
+    while(!(SPSR & (1<<SPIF))); 
+
+    return SPDR;
 }
 
 int main (void)
 {
-    // Enable Interrupts
+    // Global interrupts
     sei();
 
     // Enable LCD
     lcd_init(LCD_DISP_ON_CURSOR_BLINK);
 
     // Hello!
-    lcd_puts("Hello World!");
-
+    lcd_puts("SPI Example\nPress Button");
 
     // Setup LED IO pins
-    DDRD |= _BV(PD0);
+    DDRD |= _BV(PD0) | _BV(PD1);
 
-    // Setup SPI IO pins (MOSI and SCK)
-    DDRD |= _BV(PD3) | _BV(PD4);
+    // Button Interrupts
+    PCICR |= _BV(PCIE0);
+    PCMSK0 |= _BV(PCINT0) | _BV(PCINT1);
 
-    // Enable alternate SPI (MISO_A, MOSI_A, etc)
-    MCUCR |= _BV(SPIPS);
+    // SPI init
+    init_spi_master();
 
-    // Enable SPI
-    SPCR |= _BV(SPIE) | _BV(SPE) | _BV(MSTR) | _BV(SPR0);
+    uint8_t msg = 0x01;
 
+    // Slave Select
+    DDRC |= _BV(PC1);
+    PORTC &= ~_BV(PC1);
 
     while(1)
     {
-        _delay_ms(500);
-        PORTD |= _BV(PD0);
-        SPDR = 0x10;
+        if( bit_is_set(BUTTON_STATES, 0))
+        {
+            uint8_t rec = spi_message(msg);
+            msg++;
 
-        _delay_ms(100);
-        PORTD &= ~_BV(PD0);
+            lcd_clrscr();
+            char buffer[16];
+            sprintf(buffer, "val: %x %x", msg, rec);
+            lcd_puts(buffer);
+            //_delay_ms(1000);
+
+            BUTTON_STATES &= ~_BV(0);
+        }
+
+        PORTD ^= _BV(PD0);
+        _delay_ms(200);
     }
 }
 
