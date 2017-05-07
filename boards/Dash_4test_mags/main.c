83fed70a3a072ac470fd272dfb1e1c24df776a04
MargaretRosner <rosner.margaret@gmail.com>
2017-05-07 19:09:12 -0400

    code

--- boards/Dash_4test_mags/main.c
+++ boards/Dash_4test_mags/main.c
@@ -1,8 +1,8 @@
 #define F_CPU (4000000L)
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
 #include "can_api.h"
+
 
 ISR(CAN_INT_vect)
 {
@@ -12,11 +12,8 @@ ISR(CAN_INT_vect)
     // msg now contains the first byte of the CAN message.
     // Repeat this call to receive the other bytes.
     uint8_t msg = CANMSG;
-
-    
     if (msg == 0xF1){
-      PORTC ^= _BV(PC6);
-
+      PORTB ^= _BV(PB4);
     }
 
     // Set the chip to wait for another message.
@@ -24,34 +21,27 @@ ISR(CAN_INT_vect)
 
 }
 
+
 int main (void) {
-    sei();
-    /* Most basic CAN transmission.
-     * Sends the bytes 0x11, 0x66 and
-     * 0x0a and then quits. */
 
     // Initialize CAN
     CAN_init(0, 0);
 
     // Set the array msg to contain 3 bytes
-    uint8_t msg[] = {0xF1};
 
     // Transmit message
-    //CAN_Tx( 0, IDT_GLOBAL, IDT_GLOBAL_L, msg );
     CAN_Rx(0, IDT_GLOBAL, IDT_GLOBAL_L, IDM_single);
-
-
-
     // Set PE1 to output
     // Use pin 10 to light up an LED
-    DDRC |= _BV(PC6);
-    DDRC |= _BV(PC0);
+    DDRB |= _BV(PB4);
 
     while(1) {
-      PORTC ^= _BV(PC0);
-      //send message to the throttle board
-      // CAN_Tx( 0, IDT_GLOBAL, IDT_GLOBAL_L, msg );
-      // PORTC ^= _BV(PC0); //D1
-      _delay_ms(200);
+        // // Toggle PE1 (pin 10)
+        // // Toggles power to pin 10 to create a "blink"
+        // PORTE ^= _BV(PE1);
+        //
+        //
+        // // Give a delay to the toggle so it doesn't infinitely toggle
+        // _delay_ms(500);
     }
 }
--- boards/Throttle_Brake/main.c
+++ boards/Throttle_Brake/main.c
@@ -1,37 +1,56 @@
 #define F_CPU (4000000L)
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 #include "can_api.h"
+//
+// ISR(CAN_INT_vect)
+// {
+//     // Reset CANPAGE (A necessary step; don't worry about it
+//     CANPAGE = 0x00;
+//
+//     // msg now contains the first byte of the CAN message.
+//     // Repeat this call to receive the other bytes.
+//     uint8_t msg = CANMSG;
+//
+//     PORTC ^= _BV(PC7);
+//
+//     CAN_Rx(0, IDT_GLOBAL, IDT_GLOBAL_L, IDM_single);
+//   }
 
 
 int main (void) {
-
+  //sei();
   /* Most basic CAN transmission.
    * Sends the bytes 0x11, 0x66 and
    * 0x0a and then quits. */
 
-  // Initialize CAN
-  CAN_init(0, 0);
+    // Initialize CAN
+    CAN_init(0, 0);
 
-  // Set the array msg to contain 3 bytes
-  uint8_t msg[] = {0xF1};
+    // Set the array msg to contain 3 bytes
+    uint8_t msg[] = {0xF1};
 
-
-  // Transmit message
+    //CAN_Rx(0, IDT_GLOBAL, IDT_GLOBAL_L, IDM_single);
+    CAN_Tx(0, IDT_GLOBAL, IDT_GLOBAL_L, msg );
+    // Transmit message
 
 
     // Set PE1 to output
     // Use pin 10 to light up an LED
+    DDRC |= _BV(PC7);
     DDRB |= _BV(PB2);
+
 
     while(1) {
         // Toggle PE1 (pin 10)
         // Toggles power to pin 10 to create a "blink"
-        CAN_Tx( 0, IDT_GLOBAL, IDT_GLOBAL_L, msg );
-        PORTB ^= _BV(PB2);
+        //sends CAN Message to the Transom
+        //CAN_Tx(0, IDT_GLOBAL, IDT_GLOBAL_L, msg );
+        PORTB ^= _BV(PB2); //Dp1
         _delay_ms(500);
+      }
 
         // Give a delay to the toggle so it doesn't infinitely toggle
 
     }
-}
