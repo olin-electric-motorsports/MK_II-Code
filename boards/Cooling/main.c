103004e08030a9c954be8eaa1ffd7dc09395aaf6
Riley Chapman <rlychpmn@gmail.com>
2017-06-03 17:11:51 -0400

    succesful compile and updated to run both pwm pins

--- boards/Cooling/main.c
+++ boards/Cooling/main.c
@@ -2,24 +2,22 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
-#include "ds18b20.h"
 int main()
 {
   //fan is OC1A. Pump is OC1B
 
   //output PWM Controls Pump!
-  TCCR1A |= _BV(COM1B1); //
-  TCCR1A |= _BV(COM1B0);
+  TCCR1A |= _BV(COM1B1); 
+  TCCR1A |= _BV(COM1B0); //toggle COM1B1 on output compare match
   TCCR1A |= _BV(COM1A1);
-  TCCR1A |= _BV(COM1A0);
-  TCCR1A |= _BV(WGM10);
-  TCCR1B |= _BV(WGM12);
-  TCCR1A &= ~ _BV(WGM11);
+  TCCR1A |= _BV(COM1A0); //toggle COM1A1 on output compare match
+  TCCR1A |= _BV(WGM10) | _BV(WGM11) | _BV(WGM12); //clear counter on compare match with ICR1 
+  TCCR1B |= 1; //CSn2 = 0 CSn1 = 0 CSn0 = 1
+  ICR1 = 0xFF; //clear counter at max
   DDRD |= _BV(PD2); // fan
   DDRC |= _BV(PC1); //pump
   OCR1B = 128; // half of a byte in decimal notation
-  OCR1A = 128;
-  TCCR1B |= 5; //CSn2 = 1 CSn1 = 0 CSn0 = 1
+  OCR1A = 1000;
   while(1)
   {
   }
