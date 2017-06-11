632ae540d89c52b66c8aae9ede5f0d6a5a29e197
Riley Chapman <rlychpmn@gmail.com>
2017-06-11 13:49:49 -0400

    works good enough for now

--- boards/Read_CAN/read_can.py
+++ boards/Read_CAN/read_can.py
@@ -1,17 +1,50 @@
 import serial
+import time
+
+class Message(object):
+    def __init__(self, can_id, bytes, line):
+        self.can_id = can_id
+        self.bytes = bytes
+        self.time = time.gmtime()
+        self.line = line
+
 
 class CAN(object):
-	def __init__(self, port):
-		self.ser = serial.Serial(port, 115200)
-		self.ser.readline()
+    def __init__(self, port):
+        self.ser = serial.Serial(port, 115200)
+        self.ser.readline()
+        self.message_dict = {}
 
-	def run(self):
-		while True:
-			line = self.ser.readline()
-			can_id = line[4:8]
-			print line
+    def run(self):
+        while True:
+            self.display()
+            line = self.ser.readline()
+            can_id = line[4:8]
+            message = line.split("MSG:", 1)[1]
+            bytes = message.split(",")[:8]
+            #print line
+            if can_id in self.message_dict.keys():
+                #update message in dictionary
+                self.message_dict[can_id].time = time.gmtime()
+                self.message_dict[can_id].bytes = bytes
+                self.message_dict[can_id].line = line
+            else:
+                #add message to dictionary
+                self.message_dict[can_id] = Message(can_id,bytes,line)
+
+    def display(self):
+        for key in self.message_dict:
+            #v_list =  [("%0.3f" % cell.voltage)+"V "+("%0.3f" % cell.temp)+"C |" for cell in segment]
+            #string = " ".join(str(x) for x in v_list)
+            #print str(segment[0].segment) + ": "+("%0.3f" % segment[0].vref2)+"Vref2 |" + string
+
+            print self.message_dict[key].line
+
+        print "-----------------------------"
+
+
 
 
 if __name__ == '__main__':
-	can = CAN('/dev/cu.usbmodem14121')
+    can = CAN('/dev/cu.usbmodem14121')
     can.run()
