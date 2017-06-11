c54a1b5a106b57452840a278927f0d700d761b6e
Byron Wasti <byron.wasti@gmail.com>
2017-06-11 16:39:42 -0400

    Shutdown sense

--- boards/Dashboard/main.c
+++ boards/Dashboard/main.c
@@ -1,5 +1,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "can_api.h"
 #include "lcd.h"
@@ -23,10 +25,11 @@
 
 /* Global Variables */
 volatile uint8_t gFlags = 0x00;
-uint8_t gFlags_old = 0x00;
+uint8_t gFlags_old = 0xFF;
 uint8_t gCANMessage[8] = { 0, 0, 0, 0,
                            0, 0, 0, 0 };
 volatile uint8_t gR2DTimeout = 0x00;
+
 
 /* Interrupts */
 ISR(CAN_INT_vect) {
@@ -144,6 +147,26 @@ void initTimer(void) {
     OCR0A = 0xFF;
 }
 
+void checkShutdownState(void) {
+    if (bit_is_set(PINC, PC0)) {
+        gCANMessage[5] = 0xFF;
+    } else {
+        gCANMessage[5] = 0x00;
+    }
+
+    if (bit_is_set(PINC, PC1)) {
+        gCANMessage[6] = 0xFF;
+    } else {
+        gCANMessage[6] = 0x00;
+    }
+
+    if (bit_is_set(PIND, PD7)) {
+        gCANMessage[7] = 0xFF;
+    } else {
+        gCANMessage[7] = 0x00;
+    }
+}
+
 void updateStateFromFlags(void) {
     uint8_t diff = gFlags^gFlags_old;
 
@@ -180,9 +203,9 @@ int main(void) {
     DDRD |= _BV(R2D);
     DDRB |= _BV(PB4) | _BV(PB6) | _BV(PB7);
 
-    // Sense Start button
+    // Pin change interrupts
     PCICR |= _BV(PCIE0);
-    PCMSK0 |= _BV(PCINT5);
+    PCMSK0 |= _BV(PCINT5); // PB5
 
     // Set up our CAN timer
     initTimer();
@@ -210,6 +233,8 @@ int main(void) {
     
     // Our favorite infinite loop!
     while(1) {
+        checkShutdownState();
+
         if (gFlags != gFlags_old) {
             updateStateFromFlags();
             gFlags_old = gFlags;
