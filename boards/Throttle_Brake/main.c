28484e3d4c143f493b585d96a87aec98fee3ba4d
nathanieltan <nathaniel98.tan@gmail.com>
2017-06-13 18:37:41 -0400

    Throttle code now enables inverter properly

--- boards/Throttle_Brake/main.c
+++ boards/Throttle_Brake/main.c
@@ -4,6 +4,9 @@
 #include <avr/interrupt.h>
 #include "can_api.h"
 
+// Throttle1 min value is 645, max is 832, full range is 187
+// Throttle2 min value is 436, max is 0x02 0x40 576, full range is 140
+
 // Throttle 1Out is pin 17, which is ADC8 which is MUX 01000 = 0x08
 // Throttle 1Out is pin 18, which is ADC9 which is MUX 01001 = 0x09
 
@@ -16,11 +19,9 @@ uint8_t throttleMin;  // our min throttle value, found by experimentation
 uint8_t throttleMax;  // our max throttle value, found by experimentation
 uint8_t legalDeviation;  // 10% of the throttle range
 
-// ISR to flag when the brake is pressed
+
 
 ISR(PCINT2_vect){
-
-
 
   uint8_t recievedMSG[8];
   CAN_read_received(0, 8, recievedMSG);
@@ -46,12 +47,7 @@ ISR(PCINT2_vect){
     {
       startUpConditions |= 0b00000001;  // changes bit 0 to 1
     }
-    else if(recievedMSG[1]==0b00000000)
-    {
-      startUpConditions &= 0b11111110;  // changes bit 0 to 0
-    }
-  }//end AIRControl if
-
+  }
 }
 
 uint8_t brakePlausibility(uint16_t rThrottle[], uint16_t rBrake)
@@ -69,14 +65,14 @@ uint8_t throttleComparison(uint16_t rThrottle[])
   //These two if statements check if the two throttle values are within the 10% legal deviation of each other
   if((rThrottle[0] > rThrottle[1] + legalDeviation) || (rThrottle[1] > rThrottle[0] + legalDeviation) || rThrottle[0] < throttleMin || rThrottle[0] > throttleMax || rThrottle[1] < throttleMin || rThrottle[1] > throttleMax)
   {
-    return 1;
+    return 1;  // if not in legal deveiation
   }
-  // If two throttle values were in the legal devaition the original msg is returned
+  // If two throttle values were in the legal deviation the original msg is returned
   return 0;
 
 }
 
-uint8_t setInverterMSG(uint16_t rThrottle[])
+uint8_t setinverterMSG(uint16_t rThrottle[])
 {
   return 0;
 }
@@ -125,9 +121,8 @@ int main (void) {
   uint8_t msg[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
   /**** initialize the array inverterMSG ****/
-  //inialized to be disabled, direction be clockwise("foward"), torque and speed to be 0, discharge disabled,torque limits set to default EEPROM parameters
-  uint8_t inverterMSG[] = {0x00, 0x00, 0x00, 0x00, 0b00000000, 0x00, 0x00, 0x00};
-
+  //inialized to be disabled, direction be counter-clockwise("foward"), torque and speed to be 0, discharge disabled,torque limits set to default EEPROM parameters
+  uint8_t inverterMSG[] = {0x00, 0x00, 0x00, 0x00, 0b00000001, 0x00, 0x00, 0x00};
 
   DDRC |= _BV(PC7);
   DDRB |= _BV(PB2);
@@ -167,8 +162,9 @@ int main (void) {
     rBrake = ADC;
 
 
-    /********------------State Machine----------***********/
-    /** Pre-Startup State **/
+    /*==================State Machine=====================*/
+
+    /***************** Pre-Startup State *******************/
     if(state==0)
     {
       /**** Checks Brake ****/
@@ -184,53 +180,48 @@ int main (void) {
       /**** Checks if all startup conditions are met ****/
       if(startUpConditions == 0b00000111)
       {
-        state = 1;
-        msg[5] = 1;  // Says startup sequence is starting
+        state = 1;  // switches states
+        msg[4] = 1;  // Says startup sequence is starting
       }
-    }
-    /** Startup State **/
+      state = 1; // CHANGE LATER DO NOT KEEP THIS LINE
+    }//end of state 0
+
+    /********************* Startup State *************************/
     else if(state==1)
     {
-      msg[5] = 0;
+      msg[4] = 0;
+      msg[5] = 0x01;
       /**** Starts Up The Inverter ****/
-      CAN_transmit(CAN_MOB_1, 0xC0, 8, inverterMSG);  // sets inverter to be disabled(required to be enabled)
+      CAN_transmit(CAN_MOB_1, 0xC0, 8, inverterMSG);
       inverterMSG[5] = 0b00000001; // sets inverterMSG to enable inverter
-      CAN_transmit(CAN_MOB_1, 0xC0, 8, inverterMSG);  // sets inverter to be enabled
+      CAN_transmit(CAN_MOB_1, 0xC0, 8, inverterMSG);
 
       state = 2;
-    }
-    /** Post-Startup State **/
+    }//end of state 1
+
+    /******************** Post-Startup State *********************/
     else if(state==2)
     {
         if(throttleComparison(rThrottle)==1)
         {
-          inverterMSG[5] &= 0b11111110;
+          //inverterMSG[5] &= 0b11111110;
         }
-    }
-    //OCR0B = (uint8_t) (reading >> 2); //shifts the 10 bit two to the right to make 6
+        else
+        {
+          // send torque command to inverter
+        }
+    }//end of state 2
 
-
-
-    /********* CAN Communication Code **************/
-    /*
-    // statements to change the CAN Messsage of the brake
-    if (FLAG & 0x01){ // if the brake is pressed flag and set 1set message segment to 0xFF
-    msg[0] = 0xFF;
-    PORTB |= _BV(PB2); //turns on the led
-
-  }
-  else{ // if the brake is not pressed the message stays all zeros
-  msg[0] = 0x00;
-  PORTB &= ~_BV(PB2); //turns off the led
-}
-*/
-
+    /**** Programming LED Blink Code ****/
     if(ledCounter == 50)
     {
-        PORTB ^= _BV(PB6);
-        ledCounter = 0;
+    PORTB ^= _BV(PB6);
+      ledCounter = 0;
     }
     ledCounter++;
+
+    /********* CAN Communication Code **************/
+
 
     msg[0] = (uint8_t)(rThrottle[0] >> 8);
     msg[1] = (uint8_t) rThrottle[0];
@@ -238,13 +229,19 @@ int main (void) {
     msg[2] = (uint8_t)(rThrottle[1] >> 8);
     msg[3] = (uint8_t) rThrottle[1];
 
+    inverterMSG[0] = 0x05;
+    inverterMSG[1] = 0xBC;
+
     if(canCounter == 100)
     {
-      CAN_transmit(CAN_MOB_0, 0xC0, CAN_IDT_THROTTLE_L, msg);
+      CAN_transmit(CAN_MOB_0, 0x0B, CAN_IDT_THROTTLE_L, msg);
+      CAN_transmit(CAN_MOB_1, 0xC0, 8, inverterMSG);
+
       canCounter = 0;
     }
-    canCounter++;
-    //CAN_transmit(CAN_MOB_1, 0xC0, 8, inverterMSG)
+    canCounter ++;
+
+
     _delay_ms(1); //delay so the car doesn't flip shit
 
   } // end of while loop
