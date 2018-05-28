fb4a2aa2b523f4f4672cabc7bccc8915d8047e9d
Alexander Hoppe <alexanderehoppe@gmail.com>
2018-05-28 10:58:26 -0700

    BMS 2018 compiles but isoSPI may not be working

---
+++ boards/BMS_2018/DC2259_test/eeprom.h
@@ -0,0 +1,14 @@
+/************************************
+REVISION HISTORY
+$Revision: 1000 $
+$Date: 2016-1-4
+*/
+
+#define IC_ADDRESS 0xC8 // 1100100 0
+
+
+void write_eeprom(uint8_t memory_address, uint8_t *tx_data, uint8_t data_len);
+void read_eeprom(uint8_t memory_address, uint8_t *rx_data, uint8_t data_len);
+void write_i2c_eeprom(uint8_t total_ic, uint8_t address, uint8_t command, uint8_t *data, uint8_t data_len);
+uint8_t read_i2c_eeprom(uint8_t total_ic, uint8_t address, uint8_t command, uint8_t *rx_data, uint8_t data_len);
+
